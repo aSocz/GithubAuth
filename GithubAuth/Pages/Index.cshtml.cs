1fad6007f57e48d710e4b441c55cd01895931728
Albert Soczewski <albert.soczewski@gmail.com>
2018-10-20 15:32:29 +0200

    Adds example implementation

--- GithubAuth/Pages/Index.cshtml.cs
+++ GithubAuth/Pages/Index.cshtml.cs
@@ -1,17 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Octokit;
+using Octokit.Internal;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace GithubAuth.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+        public string GitHubAvatar { get; set; }
 
+        public string GitHubLogin { get; set; }
+
+        public string GitHubName { get; set; }
+
+        public string GitHubUrl { get; set; }
+
+        public IReadOnlyList<Repository> Repositories { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                GitHubName = User.FindFirst(c => c.Type == ClaimTypes.Name)?.Value;
+                GitHubLogin = User.FindFirst(c => c.Type == "urn:github:login")?.Value;
+                GitHubUrl = User.FindFirst(c => c.Type == "urn:github:url")?.Value;
+                GitHubAvatar = User.FindFirst(c => c.Type == "urn:github:avatar")?.Value;
+
+
+                var accessToken = await HttpContext.GetTokenAsync("access_token");
+
+                var github = new GitHubClient(new ProductHeaderValue("AspNetCoreGitHubAuth"),
+                                              new InMemoryCredentialStore(new Credentials(accessToken)));
+                Repositories = await github.Repository.GetAllForCurrent();
+
+            }
         }
     }
 }
